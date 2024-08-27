36a7195ca3203a2133bb6a20b0b2b6818f3ce857
karatejb <ppippenkimo@gmail.com>
2024-08-27 09:07:32 +0800

    Logging for request

--- src/AspNetCore.Profiler.Mvc/Startup.cs
+++ src/AspNetCore.Profiler.Mvc/Startup.cs
@@ -24,7 +24,10 @@ namespace AspNetCore.Profiler.Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(options =>
+            {
+                options.Filters.Add<HttpRequestLogFilter>();
+            });
 
             #region Entity framework
             services.AddDbContext<DemoDbContext>(options =>
--- src/AspNetCore.Profiler.Mvc/Utils/HttpRequestExtensions.cs
+++ src/AspNetCore.Profiler.Mvc/Utils/HttpRequestExtensions.cs
@@ -1,47 +1,44 @@
-﻿using System;
-using Microsoft.AspNetCore.Http;
+namespace AspNetCore.Profiler.Mvc.Utils;
 
-namespace AspNetCore.Profiler.Mvc.Utils
+/// <summary>
+/// HttpRequest Extensions
+/// </summary>
+public static class HttpRequestExtensions
 {
     /// <summary>
-    /// HttpRequest Extensions
+    /// Check if the request is authorized to access MiniProfiler
     /// </summary>
-    public static class HttpRequestExtensions
+    /// <param name="request">HttpRequest</param>
+    /// <returns>True(OK)/False(Not allowed)</returns>
+    public static bool IsAuthorizedToMiniProfiler(this HttpRequest request)
     {
-        /// <summary>
-        /// Check if the request is authorized to access MiniProfiler
-        /// </summary>
-        /// <param name="request">HttpRequest</param>
-        /// <returns>True(OK)/False(Not allowed)</returns>
-        public static bool IsAuthorizedToMiniProfiler(this HttpRequest request)
+        if (!request.Path.ToString().StartsWith("/profiler", StringComparison.InvariantCultureIgnoreCase))
         {
-          if (!request.Path.ToString().StartsWith("/profiler", StringComparison.InvariantCultureIgnoreCase))
-            {
-                return true;
-            }
-
-            var bearerTokenPrefix = "Bearer";
-            var accessToken = string.Empty;
-            
-            var authorizationHeader = request.Headers["Authorization"].ToString();
-
-            // Get Access token from header
-            if (!string.IsNullOrEmpty(authorizationHeader) &&
-                authorizationHeader.StartsWith(bearerTokenPrefix, StringComparison.OrdinalIgnoreCase))
-            {
-                accessToken = authorizationHeader.Replace(bearerTokenPrefix, string.Empty, StringComparison.OrdinalIgnoreCase).Trim();
-            }
-            // Get Access token from cookie
-            else if (request.Cookies.TryGetValue("access-token", out accessToken))
-            { 
-            }
-            else
-            {
-                return false;
-            }
-
-            // Validate JWT
-            return AccessTokenValidator.ValidateAsync(accessToken).Result;
+            return true;
         }
+
+        var bearerTokenPrefix = "Bearer";
+        var accessToken = string.Empty;
+
+        var authorizationHeader = request.Headers["Authorization"].ToString();
+
+        // Get Access token from header
+        if (!string.IsNullOrEmpty(authorizationHeader) &&
+            authorizationHeader.StartsWith(bearerTokenPrefix, StringComparison.OrdinalIgnoreCase))
+        {
+            accessToken = authorizationHeader.Replace(bearerTokenPrefix, string.Empty, StringComparison.OrdinalIgnoreCase).Trim();
+        }
+        // Get Access token from cookie
+        else if (request.Cookies.TryGetValue("access-token", out accessToken))
+        {
+        }
+        else
+        {
+            return false;
+        }
+
+        // Validate JWT
+        return AccessTokenValidator.ValidateAsync(accessToken).Result;
     }
 }
+
